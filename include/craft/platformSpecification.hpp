3e3f5977daa6ca1afda29a33c8b4d876ce5e57fd
joshbainbridge <joshbainbridge@googlemail.com>
2014-07-27 23:19:09 +0100

    Moeved glcorearb defintion wothin apple specification

--- include/craft/platformSpecification.hpp
+++ include/craft/platformSpecification.hpp
@@ -1,10 +1,10 @@
-#define GLFW_INCLUDE_GLCOREARB
 #ifdef _WIN32
 	#define GLEW_STATIC
 	#include <GL/glew.h>
 	#include <GLFW/glfw3.h>
 	#include <windows.h>
 #elif __APPLE__
+	#define GLFW_INCLUDE_GLCOREARB
 	#include <GLFW/glfw3.h>
 	#include <unistd.h>
 	#define Sleep(x) usleep((x)*1000)
